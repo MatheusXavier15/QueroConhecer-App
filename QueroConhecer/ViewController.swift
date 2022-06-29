b5a8511167617a4098c7d92393f190f730da6e62
Matheus Xavier <matheusxavier055@gmail.com>
2022-06-29 14:58:38 -0300

    Initial Commit

---
+++ QueroConhecer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QueroConhecer
+//
+//  Created by Matheus Xavier on 29/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
